9a5a58b17774165e588de0e337d72475c82fba06
Samuel3310 <awoluowolabisamuel@gmail.com>
2024-03-20 00:02:05 +0100

    Done with the dashboard page Search filter working for both  date and facilities It is also responsive

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -4,11 +4,13 @@ import CreateAccount from "../pages/auth/CreateAccount";
 import WelcomeBack from "../pages/auth/WelcomeBack";
 import ForgottenPassword from "../pages/auth/ForgottenPassword";
 import Profile from "../pages/profile/Profile";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/createaccount" element={<CreateAccount />} />
         <Route path="/welcomeback" element={<WelcomeBack />} />
