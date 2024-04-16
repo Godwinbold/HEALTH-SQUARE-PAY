be3bdbe1cd3ea4b93fc9cdb4af8b9d91c690fdc3
Godwinbold <gordonblaze5@gmail.com>
2024-04-16 18:26:57 +0100

    AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,10 @@
 // components/Home.js
 
 import React from 'react';
-import HeaderComponent from '../Component/HeaderComponent';
+import HeaderComponent from '../components/HeaderComponent';
 import girl2 from '../assets/svg/girl 2.svg';
 import phoneScanCode from '../assets/svg/phone scan code.svg';
-import FooterComponent from '../Component/FooterComponent';
+import FooterComponent from '../components/FooterComponent';
 const picsHeight = {
 	height: '28vh'
 };
@@ -21,13 +21,13 @@ const AboutUs = () => {
 				</div>
 				<div className="block xl:flex lg:flex md:flex lg:mb-32 xl:mb-32 md:mb-16 mb-8 bg-gray-50">
 					<div className="xl:w-1/2 lg:w-1/2 md:w-1/2 w-full xl:pl-24 lg:pl-24  md:pl-24  p-8 pr-8 ">
-						<div className="text-5xl md:text-4xl font-semibold mb-8 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">Healthsquare Pay</div>
-						<div className="block text-xl md:text-lg text-gray-700 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">
+						<div className="text-5xl md:text-4xl font-semibold mb-8 text-center xl:text-right lg:text-right md:text-right">Healthsquare Pay</div>
+						<div className="block justify-evenly text-xl md:text-lg text-gray-700 text-center xl:text-right lg:text-right md:text-right">
 							<span>At Healthsquare Pay, </span><span className="font-bold">we're revolutionizing the way healthcare is accessed and paid for across Africa.</span> <span>Our innovative web application rewards healthcare consumers with points for their spending on healthcare services. These points can then be redeemed for free healthcare services, making quality healthcare more accessible and affordable for everyone.</span></div>
 					</div>
 					<div className="xl:w-1/2 lg:w-1/2 md:w-1/2 w-full xl:pl-24 lg:pl-24  md:pl-24  p-8 pr-8 ">
-						<div className="text-5xl md:text-4xl font-semibold mb-8 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">Our Vision</div>
-						<div className="block justify-evenly text-xl md:text-lg text-gray-700 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">
+						<div className="text-5xl md:text-4xl font-semibold mb-8 text-center xl:text-right lg:text-right md:text-right">Our Vision</div>
+						<div className="block justify-evenly text-xl md:text-lg text-gray-700 text-center xl:text-right lg:text-right md:text-right">
 							<span className="font-bold">Our vision is bold yet simple: </span>to become the preferred payment system for health services across different health facilities in Africa. We believe that by providing an easy-to-use platform that rewards users for their healthcare spending, we can incentivize individuals to prioritize their health and well-being while also alleviating financial burdens associated with medical expenses.</div>
 					</div>
 				</div>
@@ -36,8 +36,8 @@ const AboutUs = () => {
 						<img className="xl:w-5/6 lg:w-5/6 md:w-full w-full"src={phoneScanCode }/>
 					</div>
 					<div className="xl:w-1/2 lg:w-1/2 md:w-1/2 w-full xl:pr-24 lg:pr-24 md:pr-24 p-8 xl:pl-8 lg:pl-8 md:pl-8">
-						<p className="xl:text-3xl lg:text-3xl md:text-md text-sm text-gray-700 font-bold mb-8 sm:mb-2 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">At Healthsquare Pay, we are committed to fostering a healthier Africa by empowering individuals to take control of their healthcare journey. Whether you're seeking routine check-ups, specialized treatments, or emergency care, Healthsquare Pay is here to support you every step of the way.</p>
-						<p className="xl:text-xl lg:text-xl md:text-sm text-xs mb-10 sm:mb-3 text-align-center xl:text-align-right lg:text-align-right md:text-align-right">Join us as we pave the way towards a healthier, happier Africa. Together, let's make quality healthcare accessible to all.</p>
+						<p className="xl:text-3xl lg:text-3xl md:text-md text-sm text-gray-700 font-bold mb-8 sm:mb-2 xl:text-right lg:text-right md:text-left text-center">At Healthsquare Pay, we are committed to fostering a healthier Africa by empowering individuals to take control of their healthcare journey. Whether you're seeking routine check-ups, specialized treatments, or emergency care, Healthsquare Pay is here to support you every step of the way.</p>
+						<p className="xl:text-xl lg:text-xl md:text-sm text-xs mb-10 sm:mb-3 text-center xl:text-right lg:text-right md:text-left">Join us as we pave the way towards a healthier, happier Africa. Together, let's make quality healthcare accessible to all.</p>
 						<div className=" text-center w-auto text-lg mb-8 bg-customMainColor py-3 px-6 hover:bg-white hover:border hover:border-customMainColor hover:text-text-customMainColor rounded-lg">Get Stared</div>
 					</div>
 				</div>
